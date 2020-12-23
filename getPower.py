660b66ee471357d6f2cf040630ac098684713916
NigelSlack <dingle76@googlemail.com>
2020-12-23 15:26:38 +0000

    Add html front end, python server and docker files

---
+++ getPower.py
@@ -0,0 +1,126 @@
+import tensorflow.keras as kr
+import numpy as np
+import pandas as pd
+
+df = pd.read_csv('powerproduction.txt', sep=",")
+df.sort_values(by=df.columns[0])
+
+s = df["speed"].to_numpy()
+po = df["power"].to_numpy()
+
+i = 0
+ix = []
+ix2 = []
+minP = 0
+maxP = 0
+minS = 0
+maxS = 0
+
+for px in po:
+    if px == 0:
+      if s[i] > minS:
+        minS = s[i]
+    else:
+        break
+    i+=1
+
+i = 0
+for px in po:
+    if px == 0:
+        ix.append(i)
+    else:
+        maxP = s[i]
+        maxI = i
+        if s[i] > maxS:
+            maxS = s[i]
+        if minP == 0:
+            minP = s[i]
+            minI = i
+    i+=1
+
+for i in ix:
+    if i < minI or i > maxI:
+       ix2.append(i) 
+    
+s2   = np.delete(s, ix2)
+po2  = np.delete(po, ix2)
+df2  = df.drop(ix2,axis=0)
+
+s  = np.delete(s, ix)
+po = np.delete(po, ix)
+df = df.drop(ix,axis=0)
+
+nnmodel1 = kr.models.Sequential()
+nnmodel1.add(kr.layers.Dense(7, input_shape=(1,), activation='sigmoid', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
+nnmodel1.add(kr.layers.Dense(8, input_shape=(1,), activation='sigmoid', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
+nnmodel1.add(kr.layers.Dense(1, activation='linear', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
+nnmodel1.compile(kr.optimizers.Adam(lr=0.001), loss='mean_squared_error')
+nnmodel1.fit(df['speed'], df['power'], epochs=500, batch_size=10)
+
+nnmodel2 = kr.models.Sequential()
+nnmodel2.add(kr.layers.Dense(7, input_shape=(1,), activation='sigmoid', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
+nnmodel2.add(kr.layers.Dense(8, input_shape=(1,), activation='sigmoid', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
+nnmodel2.add(kr.layers.Dense(1, activation='linear', kernel_initializer="glorot_uniform", bias_initializer="glorot_uniform"))
+nnmodel2.compile(kr.optimizers.Adam(lr=0.001), loss='mean_squared_error')
+nnmodel2.fit(df2['speed'], df2['power'], epochs=500, batch_size=10)
+
+testActual = df.to_numpy()
+
+# Polynomial regression ; https://scikit-learn.org/stable/modules/linear_model.html
+from sklearn.preprocessing import PolynomialFeatures
+from sklearn.pipeline import Pipeline
+from sklearn.linear_model import LinearRegression
+import sklearn.linear_model as lin
+
+def f(x,p):
+    return p[0] + x*p[1]
+
+def predict(s):
+    return f(s,p)
+    
+s = s.reshape(-1,1)
+model2 = Pipeline([('poly', PolynomialFeatures(degree=3)),('linear', LinearRegression(fit_intercept=False))])
+
+model2.fit(s,po)
+r = model2.score(s,po)
+
+#  arr = testActual[i][0].reshape(-1,1)
+#  print("Actual - ",testActual[i][1],"  Predicted - ",model2.predict(arr))
+
+
+# flask for web app.
+import flask as fl
+from flask import request
+
+# Create a new web app.
+app = fl.Flask(__name__)
+
+# Add root route.
+@app.route("/")
+def home():
+  return app.send_static_file('getPower.html')
+
+@app.route('/api/speed',methods=['GET', 'POST'])
+def speed():
+  data = request.form.get('name', '')
+  dataFloat = float(data)
+  if (dataFloat < minS) or (dataFloat > maxS):
+    q1 = 0
+    q2 = 0
+  else: 
+    dnp = np.float32(dataFloat)
+    npa = np.array( [dnp,] )  
+    q1 = nnmodel1.predict( npa)
+    q2 = nnmodel2.predict( npa)
+  
+  result = []
+  result.append("Power excluding downtime - " + str(int(q1)) + " KwH")
+  result.append("  Power including downtime - " + str(int(q2)) + " KwH")
+  return {"value": result}  
+  
+@app.route('/api/uniform')
+def uniform():
+  result = []
+  result.append("Min - " + str(minS) + " Km/H")
+  result.append("   Max - " + str(maxS) + " Km/H")
+  return {"value": result}    
